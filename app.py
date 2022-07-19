843a03c6354cb59acb6d7d61454d63c770c7f556
Mgeethabhargava <m.gbhargava@gmail.com>
2022-07-19 09:28:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+app.secret_key = 'Alexas'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=443)
+    app.run(debug=True)
